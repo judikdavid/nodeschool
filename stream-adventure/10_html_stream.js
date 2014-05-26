bbe66c45214e432cddefbfe4c5dd6fa7cc1b9e9d
David Judik <judik.david@gmail.com>
2014-05-26 17:45:52 +0200

    html stream solution

---
+++ stream-adventure/10_html_stream.js
@@ -0,0 +1,10 @@
+var trumpet = require('trumpet');
+var tr = trumpet();
+var through = require('through');
+
+var loud = tr.select('.loud').createStream();
+loud.pipe(through(function(buf){
+  this.queue(buf.toString().toUpperCase());
+})).pipe(loud);
+
+process.stdin.pipe(tr).pipe(process.stdout);
